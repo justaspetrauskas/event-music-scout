bb00544f3efbe73b35ac44b2e2e3e2a53fdaca2c
justaspetrauskas <petrauskas.justas@gmail.com>
2026-01-19 18:20:09 +0100

    fixing buttons lciks

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
-	<div class="pointer-events-none">
+	<div class="pointer-events-auto">
 		<SpeedInsights />
-		<header class="flex justify-end items-center w-full gap-4 p-4 fixed top-0">
+		<header class="flex justify-end items-center w-full gap-4 p-4 fixed top-0 z-1">
 			<SpotifyLogin />
 			<ThemeToggle />
 		</header>
@@ -12,7 +12,7 @@
 				<div class="absolute inset-0 w-[300px] h-[300px] md:w-[450px] md:h-[450px] lg:w-[600px] lg:h-[600px] bg-accent/20 rounded-[50%] blur-3xl mx-auto my-auto" />
 			</div>
 
-			<div class="mx-auto">
+			<div class="mx-auto z-1">
 				<slot />
 			</div>
 		</main>
